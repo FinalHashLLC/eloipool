75edfcbf36a16fbf02d91836f90206858a4710bb
ahmedbodi <ABodiwala@uclan.ac.uk>
2014-10-25 16:00:21 +0100

    Update config file to support new dynamic installer

--- config.py
+++ config.py
@@ -1,22 +1,8 @@
-### Settings relating to server identity
-
-# Choose between SHA256, Scrypt, ScryptN, X11
-Algorithm = 'Scrypt'
-
-Coin = 'LTC'
-
-# JSON-RPC server for getmemorypool
-UpstreamURI = 'http://username:password@localhost:8002'
-
 # Secret username allowed to use setworkaux
 SecretUser = "ssecreteloipool"
 
 # URI to send gotwork with info for every share submission
 GotWorkURI = 'http://mergedproxyuser:mergedproxypass@127.0.0.1:8331/'
-
-StratumAddresses = (
-	('', 3334),
-)
 
 # Logging of shares:
 ShareLogging = (
@@ -32,4 +18,5 @@ ShareLogging = (
 	},
 )
 
+# Dynamically Configured Settings
 
